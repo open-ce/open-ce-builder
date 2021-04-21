3f3aeba257fdfc839f8535d802e4eee265a65462
bnemanich <brad.nemanich@ibm.com>
2021-04-21 14:50:55 -0500

    Optimize yaml loading (#19)
    * Use C based yaml parser

--- open_ce/utils.py
+++ open_ce/utils.py
@@ -66,13 +66,13 @@ def remove_version(package):
     '''Remove conda version from dependency.'''
     return package.split()[0].split("=")[0]
 
-def check_if_conda_build_exists():
+def check_if_package_exists(package):
     '''Checks if conda-build is installed and exits if it is not'''
     try:
-        pkg_resources.get_distribution('conda-build')
+        pkg_resources.get_distribution(package)
     except pkg_resources.DistributionNotFound:
-        print("Cannot find `conda_build`, please see https://github.com/open-ce/open-ce-builder#requirements"
-              " for a list of requirements.")
+        print("Cannot find `{}`, please see https://github.com/open-ce/open-ce-builder#requirements"
+              " for a list of requirements.".format(package))
         sys.exit(1)
 
 def make_schema_type(data_type,required=False):
@@ -264,15 +264,15 @@ def replace_conda_env_channels(conda_env_file, original_channel, new_channel):
     Regex 'original_channel' is replaced with 'new_channel'
     '''
     #pylint: disable=import-outside-toplevel
-    import yaml
+    import open_ce.yaml_utils
 
     with open(conda_env_file, 'r') as file_handle:
-        env_info = yaml.safe_load(file_handle)
+        env_info = open_ce.yaml_utils.load(file_handle)
 
     env_info['channels'] = [re.sub(original_channel, new_channel, channel) for channel in env_info['channels']]
 
     with open(conda_env_file, 'w') as file_handle:
-        yaml.safe_dump(env_info, file_handle)
+        open_ce.yaml_utils.dump(env_info, file_handle)
 
 def get_branch_of_tag(git_tag):
     """
---
+++ open_ce/yaml_utils.py
@@ -0,0 +1,46 @@
+"""
+# *****************************************************************
+# (C) Copyright IBM Corp. 2021. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# *****************************************************************
+"""
+
+import open_ce.utils
+
+open_ce.utils.check_if_package_exists('pyyaml')
+
+#pylint: disable=wrong-import-position, wrong-import-order
+import yaml
+
+# Determine if the yaml package contains the C loaders/dumpers. If so, use
+# the C versions. Otherwise, use the python versions.
+try:
+    safe_loader = yaml.CSafeLoader
+    dumper = yaml.CDumper
+# pylint: disable=bare-except
+except:
+    safe_loader = yaml.SafeLoader
+    dumper = yaml.Dumper
+
+def load(stream):
+    """
+    Use pyyaml's safe loader. If available, the C based version of the loader will be used.
+    """
+    return yaml.load(stream, Loader=safe_loader)
+
+def dump(data, stream=None, **kwds):
+    """
+    Use pyyaml's dumper. If available, the C based version of the dumper will be used.
+    """
+    return yaml.dump(data, stream, Dumper=dumper, **kwds)
