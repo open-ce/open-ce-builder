870bea0b2ee3503ed4a68e4aa8b7754b534715f1
Nishidha Panpaliya <npanpa23@in.ibm.com>
2021-04-22 02:38:32 -0400

    Fix tests

--- tests/build_image_test.py
+++ tests/build_image_test.py
@@ -206,23 +206,27 @@ def test_get_runtime_image_file(mocker):
     mocker.patch('open_ce.utils.get_container_tool_ver', return_value='1709')
     path = build_image._get_runtime_image_file("docker")
     head_tail = os.path.split(path)
-    os.path.split(head_tail[0])[1] == "podman"
+    assert os.path.split(head_tail[0])[1] == "podman"
 
     mocker.patch('open_ce.utils.get_container_tool_ver', return_value='1131')
     path = build_image._get_runtime_image_file("docker")
     head_tail = os.path.split(path)
-    os.path.split(head_tail[0])[1] == "docker"
+    assert os.path.split(head_tail[0])[1] == "docker"
 
     mocker.patch('open_ce.utils.get_container_tool_ver', return_value='165')
     path = build_image._get_runtime_image_file("podman")
     head_tail = os.path.split(path)
-    os.path.split(head_tail[0])[1] == "docker"
+    assert os.path.split(head_tail[0])[1] == "docker"
 
     mocker.patch('open_ce.utils.get_container_tool_ver', return_value='205')
     path = build_image._get_runtime_image_file("podman")
     head_tail = os.path.split(path)
-    os.path.split(head_tail[0])[1] == "podman"
+    assert os.path.split(head_tail[0])[1] == "podman"
 
+    mocker.patch('open_ce.utils.get_container_tool_ver', return_value=None)
+    path = build_image._get_runtime_image_file("podman")
+    head_tail = os.path.split(path)
+    assert os.path.split(head_tail[0])[1] == "docker"
 
 
     
