a14508566b4796ef5b9f4cc1e65de5f0a758330d
Bryant Nelson <bryant.nelson@ibm.com>
2021-05-18 12:20:41 -0500

    Remove debugging.

--- open_ce/get_graph.py
+++ open_ce/get_graph.py
@@ -19,8 +19,6 @@
 
 import os
 
-import networkx as nx
-
 from open_ce.inputs import Argument
 from open_ce import graph
 from open_ce import build_env
@@ -36,29 +34,6 @@ ARGUMENTS = build_env.ARGUMENTS + \
 def export_graph(args):
     '''Entry Function'''
     build_tree = build_env.construct_build_tree(args)
-
-    #sub_tree_nodes = {node for initial_node in build_tree._initial_nodes for node in nx.descendants(build_tree._tree, initial_node)}
-    #sub_tree_nodes = {node for initial_node in build_tree._initial_nodes for node in build_tree._tree.predecessors(initial_node)}
-    #sub_tree_nodes = {node for initial_node in build_tree._initial_nodes for node in build_tree._tree.successors(initial_node)}
-    #sub_tree_nodes = {node for node in sub_tree_nodes for package in node.packages if 'python' not in package}
-    #sub_tree_nodes.update(build_tree._initial_nodes)
-
-    # get all paths from estimator to base to see if there's more than just the direct.
-    sub_tree_nodes = set()
-    for source_node in {node for node in build_tree._initial_nodes for package in node.packages if "py-lightgbm-base" in package}:
-        print("Source output: ", source_node.build_command.output_files)
-        for dest_node in {node for node in build_tree._initial_nodes for package in node.packages if "openmpi" in package}:
-            print("Destination output: ", dest_node.build_command.output_files)
-            paths = nx.all_simple_paths(build_tree._tree, source_node, dest_node)
-            for path in paths:
-                print(path)
-            sub_tree_nodes.update({node for path in paths for node in path})
-        print()
-        print("-----------")
-        print()
-
-
-    sub_tree = build_tree._tree.subgraph(sub_tree_nodes)
 
     graph.export_image(build_tree._tree, os.path.join(args.output_folder, "graph.png"), args.width, args.height) # pylint: disable=protected-access
 
