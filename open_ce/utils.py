3a13c32bc6615e534be58b1c2fa22c11956278ea
Deepali Chourasia <deepch23@us.ibm.com>
2021-11-23 00:34:36 -0500

    Use LooseVersion to get correct sorting for the versions

--- open_ce/utils.py
+++ open_ce/utils.py
@@ -27,7 +27,7 @@ import multiprocessing as mp
 import pkg_resources
 from open_ce.errors import OpenCEError, Error, show_warning, log
 from open_ce import inputs
-
+from distutils.version import LooseVersion
 
 DEFAULT_BUILD_TYPES = "cpu,cuda"
 SUPPORTED_BUILD_TYPES = DEFAULT_BUILD_TYPES
@@ -309,7 +309,7 @@ def get_branch_of_tag(git_tag):
     # Clean branches and sort so that highest release version number is last
     possible_branches = [possible_branch.replace('*','').strip() for possible_branch in possible_branches]
     possible_branches = list(filter(lambda x: x == "remotes/origin/main" or x == "remotes/origin/master" or
-                               x.startswith("remotes/origin/r"), sorted(possible_branches)))
+                               x.startswith("remotes/origin/r"), sorted(possible_branches, key=LooseVersion)))
 
     return possible_branches[1] if len(possible_branches) > 1 else possible_branches[0]
 
