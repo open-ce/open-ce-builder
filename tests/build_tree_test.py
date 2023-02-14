e8a099c8ba7420cd77faf40d38cbb8813ac2d81e
Nishidha Panpaliya <npanpa23@in.ibm.com>
2023-02-14 03:23:40 -0500

    Fixed pytest and pylint

--- tests/build_tree_test.py
+++ tests/build_tree_test.py
@@ -532,7 +532,7 @@ def test_build_tree_len():
 
     assert len(mock_build_tree) == 3
 
-def test_build_tree_cycle_fail(_mocker, caplog):
+def test_build_tree_cycle_fail(mocker, caplog): #pylint: disable=unused-argument
     '''
     Tests that a cycle is detected in a build_tree.
     '''
